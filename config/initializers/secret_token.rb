61dfa581fac7a277e4d18297491e5c8d792b90c1
Charles Max Wood <chuck@teachmetocode.com>
2013-07-05 21:07:38 +0000

    rails bones + rspec + devise + devise_invitable

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Relationally::Application.config.secret_key_base = '538d8d2cb1654ecfcc8b7237152efe3c7411cac0ba2f6f94ba94de0e9d4b3444fedc263f42f690c4a2ab72acac4f208e3828f35aa6a1226c583147a83efa2ef7'
